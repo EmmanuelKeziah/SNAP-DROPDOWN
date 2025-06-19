ef0085a9c71f3368630542c70c3a39bc743768da
Emmanuel Keziah <keziahema@gmail.com>
2025-06-19 01:01:56 +0100

    Update files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const featuresToggle =  document.getElementById('features-toggle');
-
 const featuresMenu = document.getElementById('features-menu');
+
 featuresToggle.addEventListener('click', () => {
     featuresMenu.classList.toggle('show');
 });
